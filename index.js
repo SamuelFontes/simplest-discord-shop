9f33b956b6035b7f859ffb5e81ecc7871b1a03e4
Samuguel <samug@archlinux.Greatek.com>
2022-08-29 19:55:13 -0300

    Added the worst way of doing a database CRUD(not actually a CRUD but it works)

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const Discord = require("discord.js"); // This was made using the 12.5.3 version
 const client = new Discord.Client();
 require('dotenv').config()
 
+// Database
+const database = require('./db');
+const Product = require('./product');
+database.sync();
+
 client.on("ready", () =>{
     console.log("Bot working");
 });
 
 client.on("message", msg =>{
+    // yandere dev style
     if(msg.content === "Hello"){
         msg.reply("World");
+    } else if(msg.content === "list products"){
+        Product.findAll().then( products => msg.reply(JSON.stringify(products)))
+    } else if(msg.content.includes("create product|")){
+
+        Product.create({
+            name: msg.content.split("|")[1],
+            price: parseFloat(msg.content.split("|")[2]),
+            description: msg.content.split("|")[3],
+        }).then(res => msg.reply(JSON.stringify(res)))
+
     }
 });
 
